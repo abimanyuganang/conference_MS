da0d25b93d7b3323bdac968fb663f66495a640b0
ganangAbimanyu <lilmanyuuu@gmail.com>
2025-11-03 23:56:18 +0800

    first_commit

---
+++ src/main/java/com/example/conferencemanagement/ConferenceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.conferencemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceManagementApplication.class, args);
+	}
+
+}
